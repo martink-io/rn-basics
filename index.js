5397f6a35851adf2e47e4a0d33cc8fa1f48e294b
martink-io <mkehayov92@gmail.com>
2018-09-26 02:14:00 +0100

    Add Bananas component

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Bananas from './Bananas';
 import LotsOfGreetings from './LotsOfGreetings';
 import {name as appName} from './app.json';
 
