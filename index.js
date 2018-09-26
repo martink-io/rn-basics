c7fef4a2d5b925e65cd983972629a5e672bbe0c2
martink-io <mkehayov92@gmail.com>
2018-09-26 02:13:01 +0100

    Add LotsOfGreetings component

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import LotsOfGreetings from './LotsOfGreetings';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
