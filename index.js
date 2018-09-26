78c6ebb524933feea61ed7dcfbd984ea27a8763b
martink-io <mkehayov92@gmail.com>
2018-09-26 02:23:10 +0100

    Add LotsOfStyles component

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import App from './App';
 import Bananas from './Bananas';
 import LotsOfGreetings from './LotsOfGreetings';
 import Blink from './Blink';
+import LotsOfStyles from './LotsOfStyles';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Blink);
+AppRegistry.registerComponent(appName, () => LotsOfStyles);
